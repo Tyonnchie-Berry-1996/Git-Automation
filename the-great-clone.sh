e569c8a83e457c7ddcd34152794fa98d11e223a7
Tyonnchie-Berry-1996 <g5fds626m7@privaterelay.appleid.com>
2025-12-03 16:54:18 -0500

    Update the-great-clone.sh

--- the-great-clone.sh
+++ the-great-clone.sh
@@ -3,10 +3,17 @@
 set -e
 
 git clone https://github.com/Tyonnchie-Berry-1996/MQL4-Scripts.git
+
 git clone https://github.com/Tyonnchie-Berry-1996/DevEnv.git 
+
 git clone https://github.com/Tyonnchie-Berry-1996/Proxmox-Home-Lab-Setup.git
+
 git clone https://github.com/Tyonnchie-Berry-1996/Container-Repo-Automation.git
+
 git clone https://github.com/Tyonnchie-Berry-1996/Scripts-For-Dev-Env.git
+
 git clone https://github.com/Tyonnchie-Berry-1996/Build-Your-Own-IPTV.git
+
 git clone https://github.com/Tyonnchie-Berry-1996/API-Scripts.git
+
 git clone https://github.com/Tyonnchie-Berry-1996/AI-Bash-Assistant-Prototype.git 
