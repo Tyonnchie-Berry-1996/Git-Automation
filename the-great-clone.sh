530d9b7d1a4ec4262d30ddc0f97860c5ac9de77e
Tyonnchie-Berry-1996 <g5fds626m7@privaterelay.appleid.com>
2025-12-03 16:50:00 -0500

    Add files via upload

---
+++ the-great-clone.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+git clone https://github.com/Tyonnchie-Berry-1996/MQL4-Scripts.git
+git clone https://github.com/Tyonnchie-Berry-1996/DevEnv.git 
+git clone https://github.com/Tyonnchie-Berry-1996/Proxmox-Home-Lab-Setup.git
+git clone https://github.com/Tyonnchie-Berry-1996/Container-Repo-Automation.git
+git clone https://github.com/Tyonnchie-Berry-1996/Scripts-For-Dev-Env.git
+git clone https://github.com/Tyonnchie-Berry-1996/Build-Your-Own-IPTV.git
+git clone https://github.com/Tyonnchie-Berry-1996/API-Scripts.git
+git clone https://github.com/Tyonnchie-Berry-1996/AI-Bash-Assistant-Prototype.git 
